92088d55f4413fb587e32124ec2063223486e6c0
Naveena Krishna <nkmungila@gmail.com>
2022-07-11 18:27:48 +0530

    first Commit

---
+++ fuzzbuzz.js
@@ -0,0 +1,25 @@
+var fizz = fizzBuzz(50)
+console.log(fizz)
+
+function fizzBuzz(num){
+    if ((typeof num) !== ('number')) {
+        
+        return "not a number"
+    }
+    let fb = []
+    for(var i = 1;i<=num;i++)
+    {
+        if(!(i%3) && !(i%5)){
+            fb.push('Fizz Buzz')
+        }
+        else if (!(i%3)){
+            fb.push('Fizz')
+        }
+        else if (!(i%5)){
+            fb.push('Buzz')
+        }
+        else fb.push(i)
+    }
+    return fb
+}
+
